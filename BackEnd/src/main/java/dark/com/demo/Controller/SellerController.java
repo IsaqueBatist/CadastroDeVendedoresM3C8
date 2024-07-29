ef8b4fb6185252f7f7c1adaef6f04f7475cb5c76
Isaque <isaquebatista400@gmail.com>
2024-07-29 16:12:14 -0300

    closes #10, #6 - Criando endpoit PUT

--- BackEnd/src/main/java/dark/com/demo/Controller/SellerController.java
+++ BackEnd/src/main/java/dark/com/demo/Controller/SellerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -67,6 +68,18 @@ public class SellerController {
                       .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Seller not found"));
       sellers.remove(seller);
       return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("sellers/{id}")
+    public ResponseEntity<Seller> update(@PathVariable Long id, @RequestBody Seller sellerUpdate){
+      Seller newSellerInfo = sellers.stream()
+                      .filter(s -> s.getId() == id)
+                      .findFirst()
+                      .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Seller not found"));
+      newSellerInfo.setName(sellerUpdate.getName());
+      newSellerInfo.setSalary(sellerUpdate.getSalary());
+      newSellerInfo.setGender(sellerUpdate.getGender());
+      return ResponseEntity.ok(newSellerInfo);
     }
   
 }
