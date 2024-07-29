0a091b5d015f94f6450a83d3b58630dbc2e7eddc
Isaque <isaquebatista400@gmail.com>
2024-07-29 16:33:01 -0300

    closes #11, closes #12, closes #13, closes #14, closes #15 - Criando Gender

--- BackEnd/src/main/java/dark/com/demo/Controller/SellerController.java
+++ BackEnd/src/main/java/dark/com/demo/Controller/SellerController.java
@@ -2,7 +2,6 @@ package dark.com.demo.Controller;
 
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
