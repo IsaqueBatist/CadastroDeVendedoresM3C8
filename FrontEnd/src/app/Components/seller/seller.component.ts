b2fdcd0125288c9cdabf9296c727029351f14ec5
Isaque <isaquebatista400@gmail.com>
2024-07-30 17:59:05 -0300

    closes #20, closes #25 - Finalizando a criacao de comopnentes obrigatorios

--- FrontEnd/src/app/Components/seller/seller.component.ts
+++ FrontEnd/src/app/Components/seller/seller.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { SellerformComponent } from "../sellerform/sellerform.component";
 
 @Component({
   selector: 'app-seller',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, SellerformComponent],
   templateUrl: './seller.component.html',
   styleUrl: './seller.component.css'
 })
---
+++ FrontEnd/src/app/Components/sellerform/sellerform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sellerform',
+  standalone: true,
+  imports: [],
+  templateUrl: './sellerform.component.html',
+  styleUrl: './sellerform.component.css'
+})
+export class SellerformComponent {
+
+}
