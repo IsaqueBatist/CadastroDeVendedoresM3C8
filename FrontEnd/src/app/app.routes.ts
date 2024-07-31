da4b9ee86f0db91691fc19f73a6ebdf661fd8939
Isaque <isaquebatista400@gmail.com>
2024-07-31 18:16:16 -0300

    closes #31 - Implementando rotas home e sellers

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SellerComponent } from './Components/seller/seller.component';
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'seller', component: SellerComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+]
 
-export const routes: Routes = [];
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+
+})
+export class AppRoutingModule { }
